ca7db939abc213382a24a4456b7e61624b1f8bbd
Liam Marrie <liam.marrie@edu.sait.ca>
2025-03-31 15:42:11 -0600

    removed setup testing variables

--- scripts/main.js
+++ scripts/main.js
@@ -53,8 +53,6 @@ function setupLights() {
 // render loop
 function animate() {
   requestAnimationFrame(animate);
-  //cube.rotation.x += 0.01;
-  //cube.rotation.y -= 0.01;
   renderer.render(scene, camera);
   stats.update(); // display fps counter
 }
